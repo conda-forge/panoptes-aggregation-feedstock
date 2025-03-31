f8189dda82f671b90dabc77376d96a84a3293f65
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-03-31 17:33:22 -0500

    rework tests, pip check

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,10 +1,10 @@
 import sys
 from subprocess import call
 
-FAIL_UNDER = "860"
+FAIL_UNDER = "86"
 COV = ["coverage"]
 RUN = ["run", "--source=panoptes_aggregation", "--branch", "-m"]
-PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long"]
+PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long", "--pyargs", "panoptes_aggregation"]
 REPORT = ["report", "--show-missing", "--skip-covered", f"--fail-under={FAIL_UNDER}"]
 
 SKIPS = [
@@ -17,11 +17,14 @@ SKIPS = [
 SKIP_OR = " or ".join(SKIPS)
 K = ["-k", f"not ({SKIP_OR})"]
 
+def do(args: list[str]):
+    print(">>>", *args, flush=True)
+    return call(args)
 
 if __name__ == "__main__":
     sys.exit(
         # run the tests
-        call([*COV, *RUN, *PYTEST, *K])
+        do([*COV, *RUN, *PYTEST, *K])
         # maybe run coverage
-        or call([*COV, *REPORT])
+        or do([*COV, *REPORT])
     )
